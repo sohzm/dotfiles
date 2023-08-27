0cb9cfaaebc3faf34d436f9c0d115d94e8b3b753
Soham <sohxm7@gmail.com>
2023-08-28 02:33:28 +0530

    jajk

---
+++ .config/polybar/launch.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/enc bash
+
+killall -q polybar
+
+echo " ::: " | tee -a /tmp/polybar1.log /tmp/polybar2.log
+polybar example 2>&1 | tee -a /tmp/polybar1.log & disown
+
+echo "Bars launched..."
